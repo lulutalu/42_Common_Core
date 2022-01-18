d2ca25c21216d39c1e74ac51872bc66247b13ccd
Loic Dubouloz <lduboulo@c1r12s4.42lausanne.ch>
2022-01-18 16:42:20 +0100

    Isometric projection done with simple trigonometric formula, need bit of cleaning and now we need 3D translation to 2D

--- fdf/utils/main.c
+++ fdf/utils/main.c
@@ -6,7 +6,7 @@
 /*   By: lduboulo <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 20:06:12 by lduboulo          #+#    #+#             */
-/*   Updated: 2022/01/16 22:38:20 by lduboulo         ###   ########.fr       */
+/*   Updated: 2022/01/18 16:41:05 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,41 +47,63 @@ int	main(int argc, char **argv)
 	//To stop loop use txt.nbline - 1 with map.y and map.nb for map.x
 	map.y = 0;
 	map.x = 0;
-	printf("Res x : %f\n", res.x);
-	res.x_scale = res.x / map.nb;
-	res.y_scale = res.y / (txt.nbline - 1);
-	printf("Scale x : %f Scale y : %f\n", res.x_scale, res.y_scale);
+	res.x_scale = 40; //res.x / map.nb;
+	res.y_scale = 40; //(res.y + 100) / (txt.nbline - 1);
+	start.x = res.x / 2;
+	start.y = 300;
 	while (map.y < (txt.nbline - 1))
 	{
-		start.y = (map.y * res.y_scale) + (res.y_scale / 2);
+		final.x = start.x + fabs((map.nb - 1) * res.x_scale * cos(60));
+		final.y = start.y + fabs((map.nb - 1) * res.y_scale * sin(60));
+		print_line(mlx, start, final);
+		map.y++;
+		start.x = (res.x / 2) - fabs(map.y * (res.x_scale * cos(60)));
+		start.y = 300 + fabs(map.y * (res.y_scale * sin(60)));
+	}
+	start.x = res.x / 2;
+	start.y = 300;
+	map.x = 0;
+	while (map.x < map.nb)
+	{
+		final.x = start.x - fabs((txt.nbline - 2) * res.x_scale * cos(60));
+		final.y = start.y + fabs((txt.nbline - 2) * res.y_scale * sin(60));
+		print_line(mlx, start, final);
+		map.x++;
+		start.x = (res.x / 2) + fabs(map.x * (res.x_scale * cos(60)));
+		start.y = 300 + fabs(map.x * (res.y_scale * sin(60)));
+	}
+	/*x_proj = sqrt(3) * (1 / sqrt(6));
+	y_proj = (1 / sqrt(6));
+	printf("Projection X : %f\nProjection Y : %f\n", x_proj, y_proj);
+	while (map.y < (txt.nbline - 1))
+	{
+		start.y = ((map.y * res.y_scale) + (res.y_scale / 2)) * y_proj;
 		final.y = start.y;
 		map.x = 0;
 		while (map.x < (map.nb - 1))
 		{
-			start.x = (map.x * res.x_scale) + (res.x_scale / 2);
+			start.x = ((map.x * res.x_scale) + (res.x_scale / 2)) * x_proj;
 			map.x++;
-			final.x = (map.x * res.x_scale) + (res.x_scale / 2);
+			final.x = ((map.x * res.x_scale) + (res.x_scale / 2)) * x_proj;
 			print_line(mlx, start, final);
 		}
 		map.y++;
 	}
-	printf("Final x %f\n", final.x);
 	map.x = 0;
 	while (map.x < map.nb)
 	{
-		start.x = (map.x * res.x_scale) + (res.x_scale / 2);
+		start.x = ((map.x * res.x_scale) + (res.x_scale / 2)) * x_proj;
 		final.x = start.x;
 		map.y = 0;
 		while (map.y < (txt.nbline - 2))
 		{
-			start.y = (map.y * res.y_scale) + (res.y_scale / 2);
+			start.y = ((map.y * res.y_scale) + (res.y_scale / 2)) * y_proj;
 			map.y++;
-			final.y = (map.y * res.y_scale) + (res.y_scale / 2);
+			final.y = ((map.y * res.y_scale) + (res.y_scale / 2)) * y_proj;
 			print_line(mlx, start, final);
 		}
 		map.x++;
-	}
-	printf("Final y %f\n", final.y);
+	}*/
 	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, mlx.img, 0, 0);
 	mlx_loop(mlx.mlx);
 	return (1);
